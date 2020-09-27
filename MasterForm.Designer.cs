f91e339771ad4262036afa858a452c2cfc3f995e
ExecutionByFork <20407804+executionByFork@users.noreply.github.com>
2020-09-27 01:23:06 -0500

    Make readme editor text not bolded

--- MasterForm.Designer.cs
+++ MasterForm.Designer.cs
@@ -436,6 +436,7 @@
             this.readmeTxt.AcceptsTab = true;
             this.readmeTxt.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.readmeTxt.Enabled = false;
+            this.readmeTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.readmeTxt.Location = new System.Drawing.Point(21, 37);
             this.readmeTxt.Multiline = true;
             this.readmeTxt.Name = "readmeTxt";
